105ce9b49de4a4d9d00aa0adbd51e535b28ad536
rollehenrik-a11y <rollehenrik@gmail.com>
2025-10-06 12:12:14 +0200

    Add files via upload
    Tilføje alarm.h og fire.h

---
+++ nrf_sed/nrf54l15udp/include/alarm.h
@@ -0,0 +1,7 @@
+#ifndef ALARM_H
+#define ALARM_H
+
+void alarm_init(void);
+void alarm_trigger(void);
+
+#endif // ALARM_H
---
+++ nrf_sed/nrf54l15udp/include/fire.h
@@ -0,0 +1,9 @@
+#ifndef FIRE_H
+#define FIRE_H
+
+#include <openthread/instance.h>
+
+// Init knap og UDP afsendelse
+void fire_init(otInstance *instance);
+
+#endif // FIRE_H
