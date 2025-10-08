02d4bd0ea84de0de4f1812ff35bd8383d1f1ae0a
rollehenrik-a11y <rollehenrik@gmail.com>
2025-10-08 08:40:18 +0200

    Update alarm.c

--- nrf_sed/nrf54l15udp/src/alarm.c
+++ nrf_sed/nrf54l15udp/src/alarm.c
@@ -1,4 +1,4 @@
-#include "alarm.h"
+#include "alarm_test.h"
 #include <zephyr/kernel.h>
 #include <zephyr/device.h>
 #include <zephyr/drivers/gpio.h>
@@ -7,24 +7,24 @@
 #define LED_NODE DT_ALIAS(led0)
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED_NODE, gpios);
 
-void alarm_init(void)
+void alarm_test_init(void)
 {
     if (!device_is_ready(led.port)) {
-        printk("Alarm LED device not ready\n");
+        printk("Alarm for test - LED device not ready\n");
         return;
     }
     gpio_pin_configure_dt(&led, GPIO_OUTPUT_INACTIVE);
-    printk("Alarm initialized (LED ready)\n");
+    printk("Alarm test initialized (LED ready)\n");
 }
 
-void alarm_trigger(void)
+void alarm_test_trigger(void)
 {
-    printk("Alarm triggered - LED on!\n");
+    printk("Alarm test triggered - LED on!\n");
     gpio_pin_set_dt(&led, 1);
 
     // Sluk LED efter 10 sekunder
     k_sleep(K_SECONDS(10));
 
     gpio_pin_set_dt(&led, 0);
-    printk("Alarm ended - LED off\n");
+    printk("Alarm test ended - LED off\n");
 }
